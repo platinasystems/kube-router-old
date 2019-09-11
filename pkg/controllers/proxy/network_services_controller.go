1771248a68477b64af8de1521ac73fada61109a7
Stig Thormodsrud <stig@platinasystems.com>
2019-09-11 14:23:30 -0700

    use ip command for /32 address
    Signed-off-by: Stig Thormodsrud <stig@platinasystems.com>

--- pkg/controllers/proxy/network_services_controller.go
+++ pkg/controllers/proxy/network_services_controller.go
@@ -77,6 +77,7 @@ type ipvsCalls interface {
 
 type netlinkCalls interface {
 	ipAddrAdd(iface netlink.Link, ip string, addRoute bool) error
+	ipAddrAddNl(iface netlink.Link, ip string, addRoute bool) error
 	ipAddrDel(iface netlink.Link, ip string) error
 	prepareEndpointForDsr(containerId string, endpointIP string, vip string) error
 	getKubeDummyInterface() (netlink.Link, error)
@@ -97,12 +98,11 @@ type linuxNetworking struct {
 }
 
 func (ln *linuxNetworking) ipAddrDel(iface netlink.Link, ip string) error {
-	naddr := &netlink.Addr{IPNet: &net.IPNet{IP: net.ParseIP(ip), Mask: net.IPv4Mask(255, 255, 255, 255)}, Scope: syscall.RT_SCOPE_LINK}
-	err := netlink.AddrDel(iface, naddr)
-	if err != nil && err.Error() != IFACE_HAS_NO_ADDR {
-		glog.Errorf("Failed to verify is external ip %s is assocated with dummy interface %s due to %s",
-			naddr.IPNet.IP.String(), KUBE_DUMMY_IF, err.Error())
+	out, err := exec.Command("ip", "add", "del", ip, "dev", KUBE_DUMMY_IF).CombinedOutput()
+	if err != nil {
+		glog.Errorf("Failed to delete VIP %s configured on %s. Error: %v, Output: %s", ip, KUBE_DUMMY_IF, err, out)
 	}
+
 	// Delete VIP addition to "local" rt table also, fail silently if not found (DSR special case)
 	if err == nil {
 		out, err := exec.Command("ip", "route", "delete", "local", ip, "dev", KUBE_DUMMY_IF, "table", "local", "proto", "kernel", "scope", "host", "src",
@@ -118,6 +118,35 @@ func (ln *linuxNetworking) ipAddrDel(iface netlink.Link, ip string) error {
 // to kube-dummy-if. Also when DSR is used, used to assign VIP to dummy interface
 // inside the container.
 func (ln *linuxNetworking) ipAddrAdd(iface netlink.Link, ip string, addRoute bool) error {
+
+	out, err := exec.Command("ip", "add", "add", ip, "dev", KUBE_DUMMY_IF).CombinedOutput()
+	if err != nil {
+		glog.Errorf("Failed to add VIP %s configured on %s. Error: %v, Output: %s", ip, KUBE_DUMMY_IF, err, out)
+	}
+
+	// When a service VIP is assigned to a dummy interface and accessed from host, in some of the
+	// case Linux source IP selection logix selects VIP itself as source leading to problems
+	// to avoid this an explicit entry is added to use node IP as source IP when accessing
+	// VIP from the host. Please see https://github.com/cloudnativelabs/kube-router/issues/376
+
+	if !addRoute {
+		return nil
+	}
+
+	// TODO: netlink.RouteReplace which is replacement for below command is not working as expected. Call succeeds but
+	// route is not replaced. For now do it with command.
+	out, err = exec.Command("ip", "route", "replace", "local", ip, "dev", KUBE_DUMMY_IF, "table", "local", "proto", "kernel", "scope", "host", "src",
+		NodeIP.String(), "table", "local").CombinedOutput()
+	if err != nil {
+		glog.Errorf("Failed to replace route to service VIP %s configured on %s. Error: %v, Output: %s", ip, KUBE_DUMMY_IF, err, out)
+	}
+	return nil
+}
+
+// utility method to assign an IP to an interface. Mainly used to assign service VIP's
+// to kube-dummy-if. Also when DSR is used, used to assign VIP to dummy interface
+// inside the container.
+func (ln *linuxNetworking) ipAddrAddNl(iface netlink.Link, ip string, addRoute bool) error {
 	naddr := &netlink.Addr{IPNet: &net.IPNet{IP: net.ParseIP(ip), Mask: net.IPv4Mask(255, 255, 255, 255)}, Scope: syscall.RT_SCOPE_LINK}
 	err := netlink.AddrAdd(iface, naddr)
 	if err != nil && err.Error() != IFACE_HAS_ADDR {
@@ -1079,7 +1108,7 @@ func (ln *linuxNetworking) prepareEndpointForDsr(containerId string, endpointIP 
 	}
 
 	// assign VIP to the KUBE_TUNNEL_IF interface
-	err = ln.ipAddrAdd(tunIf, vip, false)
+	err = ln.ipAddrAddNl(tunIf, vip, false)
 	if err != nil && err.Error() != IFACE_HAS_ADDR {
 		netns.Set(hostNetworkNamespaceHandle)
 		activeNetworkNamespaceHandle, err = netns.Get()
